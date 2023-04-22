b9019c456c4ee7a16783a1dd191d661a5001c721
Ken <nontsuke.ken@gmail.com>
2023-04-22 15:12:29 -0400

    finished all function very janky tho

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let lowerExc = "";
 let compound = "";
 let cardio = "";
 let circuit = '{"circuits":[]}';
+let paused = false;
 //----------------------------------------
 
 //Button
@@ -18,7 +19,11 @@ const cancelBut = document.getElementById("cancelBut");
 const menu = document.getElementById("menu");
 const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
+const stopMenu = document.getElementById("stopMenu");
+const pauseMenu = document.getElementById("pauseMenu");
 //----------------------------------------
+stopMenu.style.display = "none"
+pauseMenu.style.display = "none"
 
 //Pages
 const countdown = document.getElementById("countdown")
@@ -31,6 +36,8 @@ const createCircuit3 = document.getElementById("createCircuit3");
 const createCircuit4 = document.getElementById("createCircuit4");
 const createCircuit5 = document.getElementById("createCircuit5");
 const circuitPage = document.getElementById("circuitPage");
+const menuPop = document.getElementById("menuPop");
+const congrate = document.getElementById("congrate");
 //----------------------------------------
 
 //Set Pages Default
@@ -41,14 +48,17 @@ createCircuit2.style.display = "none";
 createCircuit3.style.display = "none";
 createCircuit4.style.display = "none";
 createCircuit5.style.display = "none";
-countdown.style.display = "none"
-circuitPage.style.display = "block"
+countdown.style.display = "none";
+circuitPage.style.display = "none";
+menuPop.style.display = "none";
+congrate.style.display = "none";
 //----------------------------------------
 
 //bodymovin js to play Lottiefiles
 const loadAnimation = document.getElementById("loadani");
 const firstAnimation = document.getElementById("firstAnim");
 const countdAnimation = document.getElementById("countdani");
+const congrateani = document.getElementById("congrateani");
 const animItem = bodymovin.loadAnimation({
     wrapper: loadAnimation,
     animType: 'svg',
@@ -73,32 +83,40 @@ const countdAni = bodymovin.loadAnimation({
     path:'Assets/countdown.json'
 })
 
+const congratAni = bodymovin.loadAnimation({
+    wrapper: congrateani,
+    animType: 'svg',
+    loop: true,
+    autoplay: true,
+    path:'https://assets5.lottiefiles.com/packages/lf20_obhph3sh.json'
+})
+
 
 
 //----------------------------------------
 
 // Using JQUERY to handle page load with a delay because page is not heavy enough.
 $(window).on("load",function(){
-    // $(".loader-wrapper").delay(2000).slideToggle(300);
-    $(".loader-wrapper").hide();
+    $(".loader-wrapper").delay(2000).slideToggle(300);
+    // $(".loader-wrapper").hide();
 });
 //----------------------------------------
 
 // Set Delay on homepage element to change the index after 2 sec
-// setTimeout(() => {
-//     mainPage.style.zIndex = "1";
-//     if (typeof (Storage) !== "undefined") {
-//         if (localStorage.hasOwnProperty("first")) {
-//             homePage.style.display = "block";
-//              renderCircuits();
-//         } else {
-//             firstTime.style.display = "block";
-//         }
-//     } else {
-//         alert("your Browser doesn't has local storage")
-//     }
+setTimeout(() => {
+    mainPage.style.zIndex = "1";
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.hasOwnProperty("first")) {
+            homePage.style.display = "block";
+             renderCircuits();
+        } else {
+            firstTime.style.display = "block";
+        }
+    } else {
+        alert("your Browser doesn't has local storage")
+    }
 
-// }, 2400);
+}, 2400);
 //----------------------------------------
 
 //create user first circuit run only one time
@@ -118,8 +136,9 @@ let toggleExc = (excTab) => {
 }
 //----------------------------------------
 
-//Begin Circuit
 
+let timer;
+//Begin Circuit
 let BeginCircit = (name, time, upper, lower, compound, cardio) => {
     console.log(name + time + upper + lower + compound + cardio)
     homePage.style.display = "none"
@@ -128,8 +147,157 @@ let BeginCircit = (name, time, upper, lower, compound, cardio) => {
     $(".countdown-wrapper").delay(3000).fadeToggle(400);
     setTimeout(() => {
         circuitPage.style.display = "block"
+        circuitStart(time, upper, lower, compound, cardio);
         countdown.style.display = "none"
+        countdAni.stop();
     },3500)
+}
+
+let uppertime = 60;
+let lowertime = 60;
+let compoundtime = 60;
+let cardiotime = 60;
+let breaktime = 60;
+const cctimeDisplay = document.getElementById("CircuitTimeDisplay");
+const excDisplay = document.getElementById("excDisplay");
+const ccDes = document.getElementById("ccDes");
+
+let checkCircuit = (set, upper, lower, compound,cardio) => {
+    if(!paused) {
+        if(uppertime > 0){
+            uppertime--;
+            if(uppertime <= 9){
+                cctimeDisplay.innerText = "00:" + "0"+uppertime;
+            }
+            else{
+                cctimeDisplay.innerText = "00:" + uppertime;
+            }
+            ccDes.innerText = DesCheck(upper)
+            excDisplay.innerText = upper
+        }
+        else if(uppertime <= 0 && lowertime > 0){
+            lowertime--;
+            if(lowertime <= 9){
+                cctimeDisplay.innerText = "00:" + "0"+lowertime;
+            }
+            else{
+                cctimeDisplay.innerText = "00:" + lowertime;
+            }
+            ccDes.innerText = DesCheck(lower)
+            excDisplay.innerText = lower
+        }
+        else if(lowertime <= 0 && compoundtime > 0){
+            compoundtime--;
+            if(compoundtime <= 9){
+                cctimeDisplay.innerText = "00:" + "0"+compoundtime;
+            }
+            else{
+                cctimeDisplay.innerText = "00:" + compoundtime;
+            }
+            ccDes.innerText = DesCheck(compound)
+            excDisplay.innerText = compound
+        }
+        else if(compoundtime <= 0 && cardiotime > 0){
+            cardiotime--;
+            if(cardiotime <= 9){
+                cctimeDisplay.innerText = "00:" + "0"+cardiotime;
+            }
+            else{
+                cctimeDisplay.innerText = "00:" + cardiotime;
+            }
+            ccDes.innerText = DesCheck(cardio)
+            excDisplay.innerText = cardio
+        }
+        else if(cardiotime <= 0 && breaktime > 0){
+            breaktime--;
+            if(breaktime <= 9){
+                cctimeDisplay.innerText = "00:" + "0"+breaktime;
+            }
+            else{
+                cctimeDisplay.innerText = "00:" + breaktime;
+            }
+            ccDes.innerText = DesCheck("")
+            excDisplay.innerText = "Break!"
+        }
+        else if(breaktime <= 0 && set > 0){
+            uppertime = 60;
+            lowertime = 60;
+            compoundtime = 60;
+            cardiotime = 60;
+            breaktime = 60;
+        }
+        else{
+            console.log("circuit complete")
+            congratualation();
+        }
+    }
+}
+
+let congratualation = () => {
+    congrate.style.display = "block"
+    setTimeout(()=>{
+        location.reload();
+    }, 2000)
+}
+
+let circuitStart = (time, upper, lower, compound, cardio) =>{
+    let set = time * 60;
+    console.log(set)
+    
+    timer = setInterval(() => {
+        
+    checkCircuit(set, upper, lower, compound,cardio)
+    --set;
+    }
+    , 1000);
+}
+
+let DesCheck = (exc) => {
+
+    let output;
+
+    switch(exc) {
+        case "Push-up":
+            output = 'Push-ups are a classic bodyweight exercise that primarily target the chest, triceps, and shoulders, by lifting and lowering your body using your arms while maintaining a plank position, and they can be modified in various ways to suit different fitness levels.'
+        break;
+        case "Tricep dip":
+            output = 'Tricep dips are an effective bodyweight exercise that target the triceps muscles, using a bench or sturdy surface for support, and they can be modified in various ways to increase or decrease the difficulty level.'
+        break;
+        case "Bent-over row":
+            output = 'Bent-over rows are a compound strength-training exercise that targets multiple muscle groups in the upper body, particularly the back, biceps, and shoulders, and involve leaning forward with a slight bend in the knees, holding a weight in each hand, and pulling the weights towards your chest while keeping your elbows close to your body.'
+        break;
+        case "Squat":
+            output = 'Squats are a fundamental strength-training exercise that targets multiple muscle groups, particularly the quadriceps, glutes, and hamstrings, and involve standing with feet shoulder-width apart, bending your knees and hips to lower your body into a sitting position, and then standing back up. Proper form, including keeping your knees in line with your toes and maintaining a neutral spine, is important to prevent injury and maximize the benefits of this exercise.'
+        break;
+        case "Superman":
+            output = 'The Superman exercise is a bodyweight exercise that targets the lower back and glute muscles, and involves lying face down on the ground with arms extended overhead and legs straight, then simultaneously lifting your arms, chest, and legs off the ground, holding briefly, and then lowering back down. This exercise can be modified by lifting only the arms or only the legs, or by using a stability ball for support.'
+        break;
+        case "Forward lunge":
+            output = 'Forward lunges are a lower body strength-training exercise that target the quadriceps, hamstrings, and glutes, and involve standing with feet hip-width apart, stepping one foot forward and bending both knees to lower your body towards the ground, keeping your front knee in line with your toes and your back knee pointing towards the floor, and then pushing back up to the starting position. This exercise can be performed with bodyweight or with added weight using dumbbells or a barbell.'
+        break;
+        case "Jumping lunge":
+            output = 'Jumping lunges are a dynamic variation of the lunge exercise that increases the intensity and adds a cardiovascular component, targeting the quadriceps, hamstrings, and glutes, and involve starting in a lunge position with one leg forward and one leg back, then jumping explosively and switching the position of your legs mid-air to land in a lunge with the opposite leg forward, and repeating for the desired number of reps or time. Proper form, including keeping your knees in line with your toes and maintaining a neutral spine, is important to prevent injury.'
+        break;
+        case "Mountain climbers":
+            output = 'Mountain climbers are a dynamic bodyweight exercise that target multiple muscle groups, including the core, shoulders, and legs, and involve starting in a plank position with your hands shoulder-width apart and your body in a straight line, then bringing one knee towards your chest, then quickly switching legs in a running-like motion, keeping your hips level and your core engaged throughout the movement. This exercise can be performed for a set number of reps or for time.'
+        break;
+        case "Thruster":
+            output = 'The thruster is a compound strength-training exercise that combines a front squat with an overhead press, targeting multiple muscle groups including the quadriceps, glutes, shoulders, and triceps, and involves holding a barbell or dumbbells at shoulder height, squatting down until your thighs are parallel to the ground, then standing up and pressing the weights overhead in one fluid motion. This exercise requires proper form and coordination to perform safely and effectively.'
+        break;
+        case "Jumping rope":
+            output = 'Jumping rope is a cardiovascular exercise that involves jumping over a fast-moving rope while maintaining proper form, including keeping your feet together and your elbows close to your sides, and landing softly on the balls of your feet to minimize impact on your joints. This exercise can be performed with a variety of jumping techniques and variations, such as single-leg jumps, double unders, or criss-crosses, and can be an effective way to improve coordination, endurance, and agility.'
+        break;
+        case "Running":
+            output = 'Running is a popular form of cardiovascular exercise that involves moving at a faster pace than walking or jogging, and can be performed indoors on a treadmill or outdoors on a track, road, or trail. Proper form, including maintaining a relaxed posture and landing mid-foot, as well as gradually increasing intensity and distance, can help prevent injury and maximize the benefits of running, such as improved cardiovascular fitness, weight management, and stress reduction.'
+        break;
+        case "Cycling":
+            output = 'Cycling is a low-impact cardiovascular exercise that can be performed indoors on a stationary bike or outdoors on a road, trail, or mountain bike, and targets multiple muscle groups including the quadriceps, hamstrings, glutes, and calves. Proper form, including maintaining a neutral spine and adjusting the resistance level to match your fitness level, can help prevent injury and maximize the benefits of cycling, such as improved cardiovascular fitness, weight management, and stress reduction.'
+        break;
+        default:
+        output = 'Great job on your workout! Remember to take breaks as needed and stay hydrated by drinking water throughout your session. Your body will thank you for it.'   
+    }
+
+    return output;
 }
 //----------------------------------------
 
@@ -309,10 +477,33 @@ menu.onclick = () =>{
 }
 
 pause.onclick = () =>{
-    console.log("pause!");
+    menuPop.style.display = "block";
+    pauseMenu.style.display = "block"
+    paused = true;
 }
 stop.onclick = () =>{
-    console.log("stop");
+    menuPop.style.display = "block";
+    stopMenu.style.display ="block";
+    paused = true;
+}
+pauseMenu.onclick = () =>{
+    menuPop.style.display = "none";
+    pauseMenu.style.display = "none";
+    paused = false;
 }
 
-renderCircuits();
+let stopY = () => {
+    console.log("stop");
+    location.reload();
+    // homePage.style.display = "block"
+    // menuPop.style.display = "none";
+    // stopMenu.style.display ="none";
+}
+
+let stopN = () => {
+    menuPop.style.display = "none";
+    stopMenu.style.display ="none";
+}
+//----------------------------------------
+
+renderCircuits();//call default
