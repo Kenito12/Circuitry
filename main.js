0307a3eb15537e95ad8b2b64fbbdec23b41e212c
Ken <nontsuke.ken@gmail.com>
2023-04-22 11:54:03 -0400

    finished style for circuitPage and stagger menu

--- main.js
+++ main.js
@@ -15,9 +15,13 @@ const timeDisplay = document.getElementById("timeDisplay");
 const circuitName = document.getElementById("circuitName");
 const firstCircuit = document.getElementById("firstCircuit");
 const cancelBut = document.getElementById("cancelBut");
+const menu = document.getElementById("menu");
+const pause = document.getElementById("pause");
+const stop = document.getElementById("stop");
 //----------------------------------------
 
 //Pages
+const countdown = document.getElementById("countdown")
 const mainPage = document.getElementById("mainPage");
 const firstTime = document.getElementById("firstTime");
 const homePage = document.getElementById("homePage");
@@ -26,21 +30,25 @@ const createCircuit2 = document.getElementById("createCircuit2");
 const createCircuit3 = document.getElementById("createCircuit3");
 const createCircuit4 = document.getElementById("createCircuit4");
 const createCircuit5 = document.getElementById("createCircuit5");
+const circuitPage = document.getElementById("circuitPage");
 //----------------------------------------
 
 //Set Pages Default
 firstTime.style.display = "none";
 homePage.style.display = "none";
-createCircuit.style.display = "block";
+createCircuit.style.display = "none";
 createCircuit2.style.display = "none";
 createCircuit3.style.display = "none";
 createCircuit4.style.display = "none";
 createCircuit5.style.display = "none";
+countdown.style.display = "none"
+circuitPage.style.display = "block"
 //----------------------------------------
 
 //bodymovin js to play Lottiefiles
-const loadAnimation = document.getElementById("loadani")
-const firstAnimation = document.getElementById("firstAnim")
+const loadAnimation = document.getElementById("loadani");
+const firstAnimation = document.getElementById("firstAnim");
+const countdAnimation = document.getElementById("countdani");
 const animItem = bodymovin.loadAnimation({
     wrapper: loadAnimation,
     animType: 'svg',
@@ -56,6 +64,17 @@ const firstAnim = bodymovin.loadAnimation({
     autoplay:true,
     path: 'https://assets10.lottiefiles.com/private_files/lf30_i5o0xxk6.json',
 })
+
+const countdAni = bodymovin.loadAnimation({
+    wrapper: countdAnimation,
+    animType: 'svg',
+    loop: false,
+    autoplay: false,
+    path:'Assets/countdown.json'
+})
+
+
+
 //----------------------------------------
 
 // Using JQUERY to handle page load with a delay because page is not heavy enough.
@@ -71,6 +90,7 @@ $(window).on("load",function(){
 //     if (typeof (Storage) !== "undefined") {
 //         if (localStorage.hasOwnProperty("first")) {
 //             homePage.style.display = "block";
+//              renderCircuits();
 //         } else {
 //             firstTime.style.display = "block";
 //         }
@@ -99,8 +119,17 @@ let toggleExc = (excTab) => {
 //----------------------------------------
 
 //Begin Circuit
-let BeginCircit = () => {
-    console.log('circuit start')
+
+let BeginCircit = (name, time, upper, lower, compound, cardio) => {
+    console.log(name + time + upper + lower + compound + cardio)
+    homePage.style.display = "none"
+    countdown.style.display = "block"
+    countdAni.play();
+    $(".countdown-wrapper").delay(3000).fadeToggle(400);
+    setTimeout(() => {
+        circuitPage.style.display = "block"
+        countdown.style.display = "none"
+    },3500)
 }
 //----------------------------------------
 
@@ -248,6 +277,7 @@ let circuitCreated = () => {
 }
 //----------------------------------------
 
+//render the Circuit from the local storage if has one, as a tab on home page
 let renderCircuits = () => {
     const tabrender = document.getElementById("tabrender");
     let circuitObj;
@@ -259,7 +289,7 @@ let renderCircuits = () => {
             let tab = `
             <div class="circuitTab" onclick="toggleWidth(this)">
                 <h2 class="circuitname">${circuitObj.circuits[i].name} <span class="time">${circuitObj.circuits[i].time}min</span></h2>
-                <div class="subBut" onclick="BeginCircit()">
+                <div class="subBut" onclick="BeginCircit('${circuitObj.circuits[i].name}','${circuitObj.circuits[i].time}', '${circuitObj.circuits[i].upper}', '${circuitObj.circuits[i].lower}', '${circuitObj.circuits[i].compound}', '${circuitObj.circuits[i].cardio}')">
                     <h2>Begin Circuit</h2>
                     <img src="Assets/arrow.png" alt="arrow.png">
                 </div>
@@ -270,3 +300,19 @@ let renderCircuits = () => {
         }
     }
 }
+//----------------------------------------
+
+//Stager Menu on CircuitPage
+menu.onclick = () =>{
+    pause.classList.toggle("pauseUp");
+    stop.classList.toggle("stopUp");
+}
+
+pause.onclick = () =>{
+    console.log("pause!");
+}
+stop.onclick = () =>{
+    console.log("stop");
+}
+
+renderCircuits();
