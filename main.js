70decb7b84617f94dc58d45bdbef369f79b92b9e
Ken <nontsuke.ken@gmail.com>
2023-04-22 09:47:31 -0400

    create circuit function finished localStorage working working on display circuit

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 //Global variables
 let ccName = "";
 let timeLimit = 0;
+let upperExc = "";
+let lowerExc = "";
+let compound = "";
+let cardio = "";
+let circuit = '{"circuits":[]}';
 //----------------------------------------
 
 //Button
@@ -18,13 +23,19 @@ const firstTime = document.getElementById("firstTime");
 const homePage = document.getElementById("homePage");
 const createCircuit = document.getElementById("createCircuit");
 const createCircuit2 = document.getElementById("createCircuit2");
+const createCircuit3 = document.getElementById("createCircuit3");
+const createCircuit4 = document.getElementById("createCircuit4");
+const createCircuit5 = document.getElementById("createCircuit5");
 //----------------------------------------
 
 //Set Pages Default
 firstTime.style.display = "none";
 homePage.style.display = "none";
-createCircuit.style.display = "none";
+createCircuit.style.display = "block";
 createCircuit2.style.display = "none";
+createCircuit3.style.display = "none";
+createCircuit4.style.display = "none";
+createCircuit5.style.display = "none";
 //----------------------------------------
 
 //bodymovin js to play Lottiefiles
@@ -49,46 +60,58 @@ const firstAnim = bodymovin.loadAnimation({
 
 // Using JQUERY to handle page load with a delay because page is not heavy enough.
 $(window).on("load",function(){
-    $(".loader-wrapper").delay(2000).slideToggle(300);
-    // $(".loader-wrapper").hide();
+    // $(".loader-wrapper").delay(2000).slideToggle(300);
+    $(".loader-wrapper").hide();
 });
 //----------------------------------------
 
 // Set Delay on homepage element to change the index after 2 sec
-setTimeout(() => {
-    mainPage.style.zIndex = "1";
-    if (typeof (Storage) !== "undefined") {
-        if (localStorage.hasOwnProperty("first")) {
-            homePage.style.display = "block";
-        } else {
-            localStorage.setItem("first", 'yes')
-            firstTime.style.display = "block";
-        }
-    } else {
-        alert("your Browser doesn't has local storage")
-    }
+// setTimeout(() => {
+//     mainPage.style.zIndex = "1";
+//     if (typeof (Storage) !== "undefined") {
+//         if (localStorage.hasOwnProperty("first")) {
+//             homePage.style.display = "block";
+//         } else {
+//             firstTime.style.display = "block";
+//         }
+//     } else {
+//         alert("your Browser doesn't has local storage")
+//     }
 
-}, 2400);
+// }, 2400);
 //----------------------------------------
 
+//create user first circuit run only one time
 firstCircuit.onclick = () =>{
+    localStorage.setItem("first", 'yes')
     firstTime.style.display = "none"
     createCircuit.style.display = "block"
 }
+//----------------------------------------
 
+//toggle classes
 let toggleWidth = (circuitTab) => {
     circuitTab.classList.toggle("expandTab");
 }
+let toggleExc = (excTab) => {
+    excTab.classList.toggle("excExpandTab");
+}
+//----------------------------------------
 
+//Begin Circuit
 let BeginCircit = () => {
     console.log('circuit start')
 }
+//----------------------------------------
 
+//Plus buton bring the create circuit page up
 plusBut.onclick = () => {
     homePage.style.display = "none"
     createCircuit.style.display = "block";
 }
+//----------------------------------------
 
+//display the time limit when user use the slider
 let displayTime = () =>{
     let time = timeSlider.value;
     if(time >= 10){
@@ -99,11 +122,15 @@ let displayTime = () =>{
     }
     timeLimit = time
 }
+//----------------------------------------
 
+//set Circuit name
 let nameCircuit = () => {
      ccName = circuitName.value;
 }
+//----------------------------------------
 
+//Check for the value from the create circuit page
 let firstNext = () =>{
     if(ccName == "" || timeLimit == 0){
         alert("please enter your Circuit name and Time Limit")
@@ -115,8 +142,104 @@ let firstNext = () =>{
         createCircuit2.style.display = "block";
     }
 }
+//----------------------------------------
 
+//cancel button function
 cancelBut.onclick = () =>{
     createCircuit.style.display = "none";
     homePage.style.display = "block";
+}
+//----------------------------------------
+
+//Back but fuctions
+let backBut = () => {
+    if(createCircuit2.style.display == "block"){
+        createCircuit.style.display = "block";
+        createCircuit2.style.display = "none";
+    }
+    else if(createCircuit3.style.display == "block"){
+        createCircuit2.style.display = "block";
+        createCircuit3.style.display = "none";
+    }
+    else if(createCircuit4.style.display == "block"){
+        createCircuit3.style.display = "block";
+        createCircuit4.style.display = "none";
+    }
+    else if(createCircuit5.style.display == "block"){
+        createCircuit4.style.display = "block";
+        createCircuit5.style.display = "none";
+    }
+}
+//----------------------------------------
+
+//Excercise Picking Funciton
+let excPick = (type, exc) => {
+    switch(type){
+        case "upperBody":
+            upperExc = exc;
+            console.log(upperExc)
+            break;
+        case "lowerBody":
+            lowerExc = exc;
+            console.log(lowerExc)
+            break;
+        case "compound":
+            compound = exc;
+            console.log(compound)
+            break;
+        case "cardio":
+            cardio = exc;
+            console.log(cardio)
+            break;
+    }
+
+    if(createCircuit2.style.display == "block"){
+        createCircuit3.style.display = "block";
+        createCircuit2.style.display = "none";
+    }
+    else if(createCircuit3.style.display == "block"){
+        createCircuit4.style.display = "block";
+        createCircuit3.style.display = "none";
+    }
+    else if(createCircuit4.style.display == "block"){
+        createCircuit5.style.display = "block";
+        createCircuit4.style.display = "none";
+    }
+    else if(createCircuit5.style.display == "block"){
+        circuitCreated();
+        homePage.style.display = "block";
+        createCircuit5.style.display = "none";
+    }
+}
+//----------------------------------------
+
+let circuitCreated = () => {
+    let circuitObj
+    if (localStorage.hasOwnProperty("circuits")) {
+        circuit = localStorage.getItem("circuits")
+        circuitObj = JSON.parse(circuit);
+        circuitObj['circuits'].push({
+            "name":ccName,
+            "time":timeLimit,
+            "upper": upperExc,
+            "lower": lowerExc,
+            "compound": compound,
+            "cardio": cardio,
+        })
+        circuit = JSON.stringify(circuitObj)
+        localStorage.setItem("circuits", circuit)
+    } else {
+        circuitObj = JSON.parse(circuit);
+        circuitObj['circuits'].push({
+            "name":ccName,
+            "time":timeLimit,
+            "upper": upperExc,
+            "lower": lowerExc,
+            "compound": compound,
+            "cardio": cardio,
+        })
+        circuit = JSON.stringify(circuitObj)
+        localStorage.setItem("circuits", circuit)
+    }
+    console.log(circuitObj);
 }
